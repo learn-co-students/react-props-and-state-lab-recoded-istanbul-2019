cf6a457d923f74d657fda381a59037f5440633cf
eliftabak <elifchorghay@gmail.com>
2020-05-27 21:19:55 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,40 @@ import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
+  state = {
       pets: [],
       filters: {
         type: 'all'
       }
     }
+  
+  onChangeType = (e) => {
+    this.setState = {
+      filters: {
+        type: e.target.value
+      }
+    }
+  }
+
+  onFindPetsClick = () => {
+    const type = this.state.filters.type
+    let URL = ''
+    if(type === 'all') {
+      URL += '/api/pets'
+    } else {
+      URL += `/api/pets?type=${type}`
+    }
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => this.setState({
+      pets : data
+    }))
+  }
+
+  onAdoptPet = (id) => {
+    const pets = this.state.pets
+    pets[pets.findIndex((e) =>
+     e.id === id)].isAdopted = true
   }
 
   render() {
@@ -24,10 +49,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.props.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
